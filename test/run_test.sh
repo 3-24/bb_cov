d4432a8353dd2d5758d3f833d8b6ddf402bb23d7
Youngseok <youngseok.choi@kaist.ac.kr>
2023-12-04 09:25:05 +0000

    Update run_test.sh

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,8 +1,12 @@
-rm out out.bc main.cc.cov
+rm main main.bc main_cov.bc main_cov
 
-opt -enable-new-pm=0 -load ../lib/bb_cov_pass.so --bbcov < main.bc -o out.bc
-clang++ out.bc -o out -L../lib -l:bb_cov_rt.a 
-./out
+gclang -O0 -g  main.cc -o main 
+get-bc main
+
+opt -enable-new-pm=0 -load ../lib/bb_cov_pass.so --bbcov -o main_cov.bc < main.bc
+llvm-dis-13 main_cov.bc
+clang++ main_cov.bc -o main_cov -L../lib -l:bb_cov_rt.a 
+./main_cov
 
 echo ""
 echo "Coverage result:"
